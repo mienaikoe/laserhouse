173e5e88ba1eb6f81cd7bb548a11a27ceeafde8f
Jesse Lee <mienaikoe@gmail.com>
2025-07-26 15:29:21 -0400

    save point

---
+++ client/LaserCanvas.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  const makerjs = require("makerjs");
+
+  const line = {
+    type: "line",
+    origin: [0, 0],
+    end: [50, 50],
+  };
+
+  const svg = makerjs.exporter.toSVG(line);
+</script>
+
+<!-- markup (zero or more items) goes here -->
+<div class="lh-canvas">
+  {@html svg}
+</div>
+
+<style>
+  /* styles go here */
+  .lh-canvas {
+    width: 500px;
+    height: 500px;
+  }
+</style>
--- client/index.js
+++ client/index.js
@@ -1,13 +1,1 @@
-const makerjs = require('makerjs');
-
-const root = document.getElementById("root");
-
-var line = { 
-  type: 'line', 
-  origin: [0, 0], 
-  end: [50, 50] 
- };
- 
-var svg = makerjs.exporter.toSVG(line);
-
-document.write(svg);
+require("./LaserCanvas.svelte");
